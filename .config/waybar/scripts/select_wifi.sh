534139a17c41ce6834681deed4c120ad73a44c35
sekmankoeng <sekmankoeng@gmail.com>
2026-09-06 23:44:56 +0800

    waybar+wofi completed mark

--- .config/waybar/scripts/select_wifi.sh
+++ .config/waybar/scripts/select_wifi.sh
@@ -19,38 +19,70 @@ wifi_list=$(nmcli -t -f SECURITY,SIGNAL,SSID device wifi list \
           else if (sig >= 40) strength_icon="󰤢";
           else if (sig >= 20) strength_icon="󰤟";
           else                strength_icon="󰤯";
-          printf "%s | %s  %d%% | %s\n", sec_icon, strength_icon, sig, ssid
+          printf "%s | %s\t%d%% | %s\n", sec_icon, strength_icon, sig, ssid
       }'
 )
 
 if [[ -z "$wifi_list" ]]; then
   notify-send "WiFi" "未扫描到WiFi网络"
-  exit 0
+  exit 1
 fi
 
 # 3. wofi选择wifi
 CHOSEN=$(echo "$wifi_list" | wofi --dmenu --prompt="💡提示：隐藏SSID直接输入即可尝试连接")
 [[ -z "$CHOSEN" ]] && exit 0
 
-# 提取SSID（取前30字符字段）
-SSID=$(echo "$CHOSEN" | awk -F' ' '{print $6}')
+# 4. 判断wofi是新填写的隐藏SSID还是选择的已扫描到的SSID
+RE='^(🔒|✅)[[:space:]]+\|[[:space:]]+([^|]+)[[:space:]]([0-9]{1,3})%[[:space:]]+\|[[:space:]]+(.*)$'
+if [[ "$CHOSEN" =~ $RE ]]; then
+  ICON="${BASH_REMATCH[1]}"
+  SSID="${BASH_REMATCH[4]}"
+else
+  SSID="$CHOSEN"
+fi
 
-# 尝试连接：已保存过该网络直接连接
+# 尝试连接：已保存过的网络直接连接
 if nmcli connection show "$SSID" >/dev/null 2>&1; then
-  nmcli connection up "$SSID"
-  notify-send "WiFi" "已连接 $SSID"
+  if ! nmcli connection up "$SSID"; then
+    notify-send -a "waybar" -u normal "WiFi" "连接失败：$SSID，该连接配置异常，请尝试删除后重新连接"
+    exit 1
+  fi
+  notify-send -a "waybar" -u normal "WiFi" "已连接 $SSID"
   exit 0
 fi
 
-# 没有保存配置 → 需要密码，wofi弹窗输入密码
-TIPS_ENTRY="<span color='#82aaff'>💡请输入WiFi密码：$SSID</span>"
-PASSWORD=$(echo ${TIPS_ENTRY} | wofi --dmenu --password)
-[[ -z "$PASSWORD" ]] && exit 0
+# 5. 分支：连接无密码的开放WiFi
+if [[ "$ICON" == "✅" ]]; then
+    if nmcli device wifi connect "$SSID"; then
+        notify-send -a "waybar" -u normal "WiFi" "连接成功：$SSID"
+    else
+        # 连接失败时，可能是nmcli新建的连接配置异常，需要清除nmcli新建的连接配置，否则下次连接同一SSID时会直接使用异常的配置尝试连接，导致连接失败。
+        if [[ -n "$SSID" ]]; then
+            nmcli connection delete "$SSID" >/dev/null 2>&1 || true
+        fi
+        notify-send -a "waybar" -u normal "WiFi" "连接失败：$SSID"
+        exit 1
+    fi
+    exit 0
+fi
+
+# 6. 分支：连接加密WiFi
+SSID_HINT="<span color='#82aaff'>💡即将连接：${SSID}</span>"
+HIDE_HINT="<span color='#82aaff'>⚠️隐藏SSID无法自动连接</span>"
+
+HINT_LIST=$( printf "%s\n%s" "$SSID_HINT" "$HIDE_HINT" )
+PASSWORD=$(echo "${HINT_LIST}" | wofi --style=${HOME}/.config/wofi/wifi_auth.css --dmenu --height=200 --password --prompt="🔑 请输入WiFi密码")
+if [[ -z "$PASSWORD" ]]; then
+  notify-send -a "waybar" -u normal "WiFi" "连接失败：$SSID，无效密码"
+  exit 1
+fi
 
 # nmcli新建连接并连接
-nmcli device wifi connect "$SSID" password "$PASSWORD"
-if [ $? -eq 0 ];then
-  notify-send "WiFi" "成功连接 $SSID"
+if nmcli device wifi connect "$SSID" password "$PASSWORD";then
+  notify-send -a "waybar" -u normal "WiFi" "连接成功：$SSID"
 else
-  notify-send -u critical "WiFi连接失败" "SSID:$SSID，密码错误或无法接入"
+  # 密码错误时，需要清除nmcli新建的连接配置，否则下次连接同一SSID时会直接使用错误的密码尝试连接，导致连接失败。
+  nmcli connection delete "$SSID" >/dev/null 2>&1 || true
+  notify-send -a "waybar" -u normal "WiFi" "连接失败：$SSID，密码错误或无法接入"
+  exit 1
 fi
