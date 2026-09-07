bb4cb1ee19d37516442beb80cbb4770ad761b0e2
sekmankoeng <sekmankoeng@gmail.com>
2026-09-08 03:23:42 +0800

    fine-tune waybar dock

--- .config/waybar/scripts/toggle_waybar.sh
+++ .config/waybar/scripts/toggle_waybar.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <Bar-ID>"
-    echo "    Bar-ID: Value Range:["topbar", "dock"]"
+set -x
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 [Bar-ID] [Action]"
+    echo "    Bar-ID: [topbar, dock]"
+    echo "    Action: [reload, toggle]"
     echo "Example: $0 dock"
     exit 1
 fi
 
 bar_id="$1"
+action="$2"
+SIG=""
+
+if [[ $action == 'reload' ]]; then
+    SIG="SIGUSR2"
+elif [[ $action == 'toggle' ]]; then
+    SIG="SIGUSR1"
+else
+    echo "Parameter error. Unkown action: ${action}"
+    exit 1
+fi
 
 CONFIG_FILE="${HOME}/.config/waybar/${bar_id}.jsonc"
 WAYBAR_ARGS=("-b" "${bar_id}" "-c" "${CONFIG_FILE}")
 
 if pgrep -f "waybar ${WAYBAR_ARGS[*]}" > /dev/null; then
-    pkill -SIGUSR2 -f "waybar ${WAYBAR_ARGS[*]}"
+    pkill -${SIG} -f "waybar ${WAYBAR_ARGS[*]}"
     exit 0
 fi
 
