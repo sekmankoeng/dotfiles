debcf80387ee4d8bc6651684a3d418c42d9dd5d4
sekmankoeng <sekmankoeng@gmail.com>
2026-09-10 02:40:45 +0800

    Refactor: use modern style of file-lock for wlogout

--- .config/niri/scripts/wlogout_toggle.sh
+++ .config/niri/scripts/wlogout_toggle.sh
@@ -3,15 +3,13 @@ set -euo pipefail
 
 CMD=(wlogout -s)
 LOCK="/tmp/wlogout.lock"
-
+echo "$$" > "${LOCK}"
 # 文件锁：防抖，防止多次快捷键并行执行
-if [[ -f "${LOCK}" ]]; then
+exec 200>"${LOCK}"
+# -n 非阻塞锁，拿不到锁就直接退出
+if ! flock -n 200; then
     exit 0
 fi
-
-touch "${LOCK}"
-# 设置脚本退出时自动清理文件锁
-trap "rm -f ${LOCK}" EXIT
 
 # 先优雅关闭，在重新启动
 if pgrep -f "${CMD[*]}" > /dev/null; then
