7ff967004c0eb5d64d88555e89e7d1d2b23f4856
sekmankoeng <sekmankoeng@gmail.com>
2026-09-09 21:27:49 +0800

    fix wlogout repeat bug

---
+++ .config/niri/scripts/toggle_wlogout.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CMD=(wlogout -s)
+LOCK="/tmp/wlogout.lock"
+
+# 文件锁：防抖，防止多次快捷键并行执行
+if [[ -f "${LOCK}" ]]; then
+    exit 0
+fi
+
+touch "${LOCK}"
+# 设置脚本退出时自动清理文件锁
+trap "rm -f ${LOCK}" EXIT
+
+# 先优雅关闭，在重新启动
+if pgrep -f "${CMD[*]}" >/dev/null; then
+    pkill -f "${CMD[*]}"
+    while pgrep -f "${CMD[*]}" >/dev/null; do
+        sleep 0.05
+    done
+fi
+
+${CMD[@]}
